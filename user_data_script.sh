983b66d32079867e0ff40f330bc42e9567eb62bc
AbdullahKL <abdullah.muqeet@koderlabs.com>
2022-05-24 22:29:34 +0500

    upadte the auto scaling .sh file

--- user_data_script.sh
+++ user_data_script.sh
@@ -1,26 +1,31 @@
-!/bin/bash -ex
+#!/bin/bash
 # output user data logs into a separate file for debugging
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+#exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 # download nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+sudo apt install curl
+#touch ~/.profile
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 # source nvm
-. /.nvm/nvm.sh
+#. /.nvm/nvm.sh
 # install node
-nvm install node
+
+#source ~/.profile
 #export NVM dir
-export NVM_DIR="/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion 
+nvm install node
 #upgrade yum
-sudo yum upgrade
+#sudo yum upgrade
 #install git
-sudo yum install git -y
+sudo apt install git
 cd /home/ubuntu
 # get source code from github
-git clone https://github.com/imabdullahkhan/auto-scaling-nodejs-app
-# git clone https://<ASSCESS_TOKEN>@github.com/<USER_NAME>/<REPO>.git
+git clone https://<ACCESS_TOKEN>/imabdullahkhan/microservice-node.git
+# git clone https://<ACCESS_TOKEN>@github.com/<USER_NAME>/<REPO>.git
 #get in project dir
-cd auto-scaling-nodejs-app
+cd microservice-node
 #give permission
 sudo chmod -R 755 .
 #install node module
