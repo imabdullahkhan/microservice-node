e1e06a0c9ec3b4ca7f2f40a4658201026ae401aa
AbdullahKL <abdullah.muqeet@koderlabs.com>
2022-05-25 22:44:55 +0500

    add the NVM PATH

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion 
 cd /home/ubuntu/microservice-node/
 npm install forever -g
 forever stopall
